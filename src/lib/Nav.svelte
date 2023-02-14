0a050d22a0c4003205c025e5d24b9255ebe1f86b
Manolo Enriquez <manolo.enriquez@icloud.com>
2023-02-14 13:38:57 +0800

    initial commit

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,3 @@
+<nav class="flex justify-center">
+  Navbar
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+	import { AppShell } from '@skeletonlabs/skeleton'
+	import Nav from '$lib/Nav.svelte'
+</script>
+
+<AppShell>
+	<!-- <svelte:fragment slot="header">
+		<Nav />
+	</svelte:fragment> -->
+	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
+	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
